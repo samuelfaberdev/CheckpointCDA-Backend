872d668b81dfede2016995d18373030daa16be6c
Samuel Faber <samuel.faber@live.fr>
2024-07-03 09:53:49 +0200

    ✨ Add Apollo Server and resolvers for Continents and Countries

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { dataSource } from "./datasource";
+import { ContinentResolver } from "./resolvers/Continents";
+import { CountryResolver } from "./resolvers/Countries";
+
+const port = 5000;
+
+async function start() {
+  const schema = await buildSchema({
+    resolvers: [CountryResolver, ContinentResolver],
+  });
+
+  const server = new ApolloServer({ schema });
+
+  await dataSource.initialize();
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port },
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
+}
+
+start();
